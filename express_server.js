68a7e2dbe7e2127b6d606b198c3fdc6dd06f0fdd
Clement Shum <clement.shum128@gmail.com>
2023-02-27 19:48:52 -0500

    first render

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -15,6 +15,11 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
